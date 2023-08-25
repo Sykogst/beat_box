856c08fbca22fe830574eea4cd36fc0d84a727ce
Sam Tran <samuelttran@gmail.com>
2023-08-24 22:12:26 -0600

    Refactor startup instance for LinkedList tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,24 +2,34 @@ require './lib/linked_list'
 require './lib/node'
 
 RSpec.describe LinkedList do
+  before(:each) do
+    @list = LinkedList.new
+  end
+
   describe '#initialize' do
     it 'LinkedList class exists' do
-      list = LinkedList.new
-      expect(list).to be_instance_of(LinkedList)
+      expect(@list).to be_instance_of(LinkedList)
     end
 
     it 'has head' do
-      list = LinkedList.new
-      expect(list.head).to be_nil
+      expect(@list.head).to be_nil
     end
   end
 
   describe '#append' do
     it 'append one thing' do
-      list = LinkedList.new
-      list.append('doop')
-      expect(list.head.data).to eq('doop')
-      expect(list.head.next_node).to be_nil
+      @list.append('doop')
+      expect(@list.head.data).to eq('doop')
+      expect(@list.head.next_node).to be_nil
     end
+    # it 'appends multiple things' do
+    # end
+  end
+
+  # describe '#count' do
+  #   it 'has a count' do
+  #     @list.count.append('doop')
+  #     expect(@list.count)
+  #   end
   end
 end
