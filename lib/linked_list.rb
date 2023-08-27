7511a11c76945ca19b76d39a2e596d442efba6ae
Sam Tran <samuelttran@gmail.com>
2023-08-26 23:05:26 -0600

    Add comments to help follow code

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -29,9 +29,12 @@ class LinkedList
     if @head == nil
       number_node = 0
     else 
+      # Initialize, node counter for single head node
       number_node = 1
       current_node = @head
+      # Iteration for beyond a single head node, stops when at tail
       while current_node.next_node != nil
+        # Increment to next node and counter
         current_node = current_node.next_node
         number_node += 1
       end
@@ -45,6 +48,7 @@ class LinkedList
       node_data
     else
       current_node = @head
+      # Iteration to push in node data, starting with head node
       while current_node.next_node != nil
         node_data << current_node.data
         current_node = current_node.next_node
