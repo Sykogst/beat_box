c6bdfdf60db8e61fb69783212c38edbbb8a4d73a
Sam Tran <samuelttran@gmail.com>
2023-08-27 17:52:27 -0600

    Add initialize tests and BeatBox class

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -1,3 +1,25 @@
 require './lib/beat_box'
 require './lib/linked_list'
 require './lib/node'
+
+RSpec.describe LinkedList do
+  before(:each) do
+    @bb = BeatBox.new
+  end
+
+  describe '#initialize' do
+    it 'BeatBox class exists' do
+      expect(@bb).to be_instance_of(BeatBox)
+    end
+
+    xit 'has list' do
+      expect(@bb.list).to be_instance_of(LinkedList)
+    end
+
+    xit 'has head' do
+      expect(@bb.list.head).to be_nil
+    end
+  end
+
+
+end
