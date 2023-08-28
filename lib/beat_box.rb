8060bee1b89709f6744cdca67e7bac626c8b6d3a
Sam Tran <samuelttran@gmail.com>
2023-08-28 16:25:28 -0600

    Expand append method in BeatBox to only append valid beats

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -5,8 +5,14 @@ class BeatBox
   end
 
   def append(more_data)
+    beats_dictionary = 'tee dee deep bop boop la na boo witt bonk rit goop sit'
+    beats_dictionary_separated = beats_dictionary.split(' ')
     more_data_separated = more_data.split(' ')
-    more_data_separated.each { |node_data| @list.append(node_data) }
+    valid_beats = more_data_separated.find_all do |node_data|
+      beats_dictionary_separated.each { |beat| node_data == beat }
+    end
+
+    valid_beats.each { |valid_beat| @list.append(valid_beat) }
   end
 
   def count
